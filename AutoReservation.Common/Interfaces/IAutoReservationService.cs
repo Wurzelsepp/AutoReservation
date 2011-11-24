21644d7247c78b7e17709bb91195ec2f30140ac1
Thomas Geiser <timgrim@gmail.com>
2011-11-24 12:44:03 +0100

    projekt

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -8,10 +8,26 @@ namespace AutoReservation.Common.Interfaces
 {
     interface IAutoReservationService
     {
-        private Dictionary<int, AutoDto> autos;
-        private Dictionary<int, ReservationDto> reservationen;
-        private Dictionary<int, KundeDto> kunden;
+        private virtual Dictionary<int, AutoDto> autos;
+        private virtual Dictionary<int, ReservationDto> reservationen;
+        private virtual Dictionary<int, KundeDto> kunden;
 
-        public List<AutoDto> GetAutos()
+        public virtual List<AutoDto> GetAutos();
+        public virtual List<ReservationDto> GetReservationen();
+        public virtual List<KundeDto> GetKunden();
+
+        public virtual AutoDto GetAuto(int key);
+        public virtual ReservationDto GetReservation(int key);
+        public virtual KundeDto GetKunde(int key);
+
+        public virtual void AddAuto(AutoDto auto);
+        public virtual void AddReservation(ReservationDto reservation);
+        public virtual void AddKunde(KundeDto kunde);
+
+        public virtual void UpdateAuto(AutoDto modified, AutoDto original);
+        public virtual void UpdateReservation(ReservationDto modified, ReservationDto original);
+        public virtual void UpdateKunde(KundeDto modified, KundeDto original);
+
+        //unvollständig
     }
 }
