a21e4e4364703134b465fff6abfc44f48fcff355
Tobias Dellenbach <tobias.dellenbach@gmx.ch>
2011-12-07 02:24:15 +0100

    Factory erstellt und Service anpassungen

---
+++ AutoReservation.Ui/Factory/Creator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoReservation.Common.Interfaces;
+using AutoReservation.Ui.Properties;
+using AutoReservation.Service.Wcf;
+
+namespace AutoReservation.Ui.Factory
+{
+    abstract class Creator
+    {
+        public abstract IAutoReservationService CreateAutoReservationServiceInstance();
+
+        public Creator GetCreatorInstance()
+        {
+            Type businessLayerType = Type.GetType(Settings.Default.BusinessLayerType);
+            return (Creator)Activator.CreateInstance(businessLayerType);
+        }
+    }
+}
---
+++ AutoReservation.Ui/Factory/LocalDataAccessCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoReservation.Common.Interfaces;
+
+namespace AutoReservation.Ui.Factory
+{
+    class LocalDataAccessCreator:Creator
+    {
+        public override IAutoReservationService CreateAutoReservationServiceInstance()
+        {
+            return GetCreatorInstance().CreateAutoReservationServiceInstance();
+        }
+    }
+}
